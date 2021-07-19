2b06a7eeab273b453e10dc668696aa0085e87a11
Summerstar2 <87510284+Summerstar2@users.noreply.github.com>
2021-07-19 12:26:37 +0530

    Update Even or Odd.py

--- Even or Odd.py
+++ Even or Odd.py
@@ -1,18 +1,15 @@
+def Continue():
+    Again=str(input("Would you like to use this program again? (Yes/No) "))
+    if Again=="No":
+        return False
+    return True
 print ("This program will take a number input from the user and provide whether the number is even or odd.")
 print ("The user can run this program as much as they want to.") 
-a=int(input("Enter a number - "))
-if a%2==1:
-    print ("This number is an odd number.")
-else:
-    print ("This number is an even number.")
 while True:
-    Again=str(input("Do you want to enter another number? (Yes/No) "))
-    if Again=="No":
-        print ("Thanks for using this program!")
-        quit()
-    if Again=="Yes":
-        a=int(input("Enter a number - "))
-        if a%2==1:
-            print ("This number is an odd number.")
-        else:
-            print ("This number is an even number.")
+    a=int(input("Enter a number - "))
+    if a%2==1:
+        print ("This number is an odd number.")
+    else:
+        print ("This number is an even number.")
+    if not Continue():
+        break
